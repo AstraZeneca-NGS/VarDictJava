9fd7dd24359d95034e1b908477a05b11b889dd63
unknown <dadamyan@chr-0809.vdev.itcwin.com>
2015-02-12 16:04:21 +0300

    javadoc comments in Region and Samrecord classes

--- src/main/java/com/astrazeneca/vardict/Region.java
+++ src/main/java/com/astrazeneca/vardict/Region.java
@@ -1,17 +1,59 @@
 package com.astrazeneca.vardict;
 
+/**
+ * Class for holding region from BED file
+ */
 public class Region {
+    /**
+     * Chromosome name
+     */
     final String chr;
+
+    /**
+     * Region start position
+     */
     final int start;
+
+    /**
+     * Region end position
+     */
     final int end;
+
+    /**
+     * Gene name
+     */
     final String gene;
+
+    /**
+     * Position to start looking for variants (for amplicon based calling)
+     */
     final int istart;
+
+    /**
+     * Position to end looking for variants (for amplicon based calling)
+     */
     final int iend;
 
+    /**
+     * Constructor for 4-column BED file line
+     * @param chr chromosome name
+     * @param start start position
+     * @param end end position
+     * @param gene gene name
+     */
     public Region(String chr, int start, int end, String gene) {
         this(chr, start, end, gene, 0, 0);
     }
 
+    /**
+     * Constructor for 8-column BED file line
+     * @param chr chromosome name
+     * @param start start position
+     * @param end end position
+     * @param gene gene name
+     * @param istart Position to start looking for variants (for amplicon based calling)
+     * @param iend Position to end looking for variants (for amplicon based calling)
+     */
     public Region(String chr, int start, int end, String gene, int istart, int iend) {
         this.chr = chr;
         this.start = start;
--- src/main/java/com/astrazeneca/vardict/Samrecord.java
+++ src/main/java/com/astrazeneca/vardict/Samrecord.java
@@ -1,17 +1,67 @@
 package com.astrazeneca.vardict;
 
+/**
+ * Class for holding aligned segment data from SAM/BAM file
+ */
 public class Samrecord {
 
+    /**
+     * Query template name
+      */
     final String qname; //0
+
+    /**
+     * Bitwise flag
+     */
     final Flag flag;//1
+
+    /**
+     * Reference sequence name
+     */
     final String rname;//2
+
+    /**
+     * 1-based leftmost mapping position of the first matching base. Set to 0 for unmapped read without coordinate
+     */
     final int position;//3
+
+    /**
+     * Mapping quality. Equal to -10*log_10 Pr(mapping position is wrong), rounded to the nearest integer.
+     * Value 255 indicates that the mapping quality is not available.
+     */
     final int mapq;//4
+
+    /**
+     * CIGAR string (see Samtools documentation)
+     */
     final String cigar; //5
+
+    /**
+     * Reference sequence name of the primary alignment of the NEXT read in the template.
+     * Set to '*' when information is not available, and to '=' if identical to rname.
+     */
     final String mrnm; //6
+
+    /**
+     * Position of the primary alignment of the NEXT read in the template. Set as 0 when the information is unavailable.
+     */
     final int mpos; //7
+
+    /**
+     * Signed observed Template LENgth (see Samtools documentation).
+     */
     final int isize; //8
+
+    /**
+     * Segment sequence
+     */
     final String querySeq; //9
+
+    /**
+     * ASCII of base QUALity plus 33 (same as the quality string in the Sanger FASTQ format).
+     * A base quality is the phred-scaled base error probability which equals -10*log10(Pr{base is wrong})
+     * This field can be a '*' when quality is not stored.
+     */
     final String queryQual; //10
 
     int recordSize;
@@ -33,11 +83,19 @@ public class Samrecord {
         queryQual = a.length > 10 ? a[10] : "";
     }
 
+    /**
+     * Is record field defined
+     * @param num - record index
+     * @return Is record defined
+     */
     public boolean isDefined(int num) {
         return num < recordSize;
     }
 
 
+    /**
+     * Class for holding bitwise flag in SAM/BAM record
+     */
     public static class Flag {
         private int flag;
 
@@ -45,18 +103,34 @@ public class Samrecord {
             this.flag = flag;
         }
 
+        /**
+         * @return indicates that the corresponding alignment line is part of a chimeric alignment.
+         */
         public boolean isSupplementaryAlignment() {
             return (flag & 0x800) != 0;
         }
 
+        /**
+         *
+         * @return next segment in the template is unmapped
+         */
         public boolean isUnmappedMate() {
             return (flag & 0x8) != 0;
         }
 
+        /**
+         *
+         * @return sequence is reverse complemented
+         */
         public boolean isReverseStrand() {
             return (flag & 0x10) != 0;
         }
 
+        /**
+         * Bit 0x100 marks the alignment not to be used in certain analyses when the tools in use are aware of this bit.
+         * It is typically used to flag alternative mappings when multiple mappings are presented in a SAM.
+         * @return Is secondary alignment
+         */
         public boolean isNotPrimaryAlignment() {
             return (flag & 0x100) != 0;
         }
